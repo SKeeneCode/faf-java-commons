20228cd5b938115cab452c8dd00f252251b9f81e
SKeeneCode <samkeene@protonmail.com>
2020-10-10 23:14:16 +0100

    add MatchmakerQueueMapPool POJO

---
+++ faf-commons-api/src/main/java/com/faforever/commons/api/dto/MatchmakerQueueMapPool.java
@@ -0,0 +1,22 @@
+package com.faforever.commons.api.dto;
+
+import com.github.jasminb.jsonapi.annotations.Relationship;
+import com.github.jasminb.jsonapi.annotations.Type;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Type("matchmakerQueueMapPool")
+public class MatchmakerQueueMapPool extends AbstractEntity {
+
+    private Double minRating;
+    private Double maxRating;
+
+    @Relationship("matchmakerQueue")
+    private MatchmakerQueue matchmakerQueue;
+
+    @Relationship("mapPool")
+    private MapPool mapPool;
+
+}
